efbda862e46941d556998290b6a2326287ff97d4
Christoffer Arvidsson <christoffer@arvidson.nu>
2019-12-03 20:07:20 +0100

    1.12019

---
+++ 1/main.py
@@ -0,0 +1,23 @@
+import sys
+import math
+
+
+def massToFuel(mass):
+    return math.floor(float(mass) / 3) - 2
+
+
+def calcTotalFuel(masses):
+    masses = map(massToFuel, masses.split())
+    return sum(masses);
+
+
+def main():
+    path = sys.argv[1]
+    f = open(path, 'r')
+    total = calcTotalFuel(f.read())
+    f.close()
+
+    print(total)
+
+if __name__=='__main__':
+    main()
