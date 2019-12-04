a541eb0748c017da682a064931560885da745adc
Christoffer Arvidsson <christoffer@arvidson.nu>
2019-12-04 21:36:28 +0100

    Delete nonworking attempt

--- 3/main.py
+++ 3/main.py
@@ -1,150 +1,55 @@
 import sys
-from pyglet.gl import *
-
-window = pyglet.window.Window()
-window.height = 700
-window.width = 1000
 
 
-def manhattanDistance(x, y):
-    return abs(x) + abs(y)
+# Moves along wire, counting steps and storing positions
+def traverse_wire(wire):
+    # Dictionary to store wire informatiosn
+    wire_dist = {}
+    # Dictionary to keep track of directions
+    directions = {'R': [1, 0], 'L': [-1, 0], 'U': [0, 1], 'D': [0, -1]}
+    x, y, stepCount = 0, 0, 0
+    for vec in wire:
+        for _ in range(int(vec[1:])):
+            direction = directions[vec[0]]
+            x += direction[0]
+            y += direction[1]
+            stepCount += 1
+            wire_dist[(x, y)] = stepCount
+    return wire_dist
 
 
-def calcMinDistance(junctions):
-    distance = -1
-    for pos in junctions:
-        (x, y) = pos
-        manHat = manhattanDistance(x, y)
-        if (distance == -1):
-            distance = manHat
-        else:
-            distance = min(distance, manHat)
-    return distance
+def solver(wire1, wire2):
+    # Traverse wires
+    wire_one = traverse_wire(wire1)
+    wire_two = traverse_wire(wire2)
 
+    # Calculate crossings
+    crossings = wire_one.keys() & wire_two.keys()
 
-def calcNextPoint(word, startX, startY):
-    direction = word[0]
-    value = int(word[1:])
-    x = startX
-    y = startY
+    fewest_steps = min(crossings, key=lambda x: wire_one[x] + wire_two[x])
+    steps = wire_one[fewest_steps] + wire_two[fewest_steps]
 
-    if direction == 'U':
-        return (x, y + value, 1)
-    elif direction == 'D':
-        return (x, y - value, -1)
-    elif direction == 'L':
-        return (x - value, y, -1)
-    else:
-        return(x + value, y, 1)
+    closest = min([intersection for intersection in crossings],
+                  key=lambda x: abs(x[0]) + abs(x[1]))
 
+    # Manhattan distance
+    distance = abs(closest[0]) + abs(closest[1])
 
-def drawPath(path, r, g, b):
-    x = 6000
-    y = 3000
-    glColor3f(r,g,b)
-    glBegin(GL_POLYGON)
-    glVertex2f(5900,2900)
-    glVertex2f(6100,2900)
-    glVertex2f(6100,3100)
-    glVertex2f(5900,3100)
-    glEnd()
-
-    glBegin(GL_LINES)
-    for word in path:
-        glVertex2f(x, y)
-        (x, y, step) = calcNextPoint(word, x, y)
-        # print("x: {x}".format(x=x))
-        # print("y: {y}".format(y=y))
-        glVertex2f(x,y)
-    glEnd()
-
-
-@window.event
-def on_draw():
-    filePath = sys.argv[1]
-    f = open(filePath, 'r')
-
-    paths = f.read().rstrip().split('\n')
-    f.close()
-
-    path1 = paths[0].split(',')
-    path2 = paths[1].split(',')
-
-    glClear(GL_COLOR_BUFFER_BIT)
-    glLoadIdentity()
-    glScalef(0.05, 0.05, 0.05)
-    drawPath(path1, 255, 0, 0)
-    drawPath(path2, 0, 255, 0)
+    return ('task 1', distance, 'task 2', steps)
 
 
 def main():
-    filePath = sys.argv[1]
-    f = open(filePath, 'r')
-
-    paths = f.read().rstrip().split('\n')
+    # Open file
+    f = open(sys.argv[1], 'r')
+    raw_data = f.read().rstrip().split('\n')
     f.close()
 
-    path1 = paths[0].split(',')
-    path2 = paths[1].split(',')
-    # print(path1)
-    # print(path2)
+    raw_wire1 = raw_data[0].split(',')
+    raw_wire2 = raw_data[1].split(',')
 
-    # Store used coordinates in set
-    coords = set([])
+    solver(raw_wire1, raw_wire2)
+    print(solver(raw_wire1, raw_wire2))
 
-    # Store junctions in set
-    junctions = set([])
-
-    x = 0
-    y = 0
-    for word in path1:
-        print(word)
-        (nextX, nextY, step) = calcNextPoint(word, x, y)
-        for xpos in range(x + step, nextX+step, step):
-            coords.add((xpos, y))
-            print("Adding ({x},{y})".format(x=xpos, y=y))
-        x = nextX
-
-        for ypos in range(y + step, nextY+step, step):
-            coords.add((x, ypos))
-            print("Adding ({x},{y})".format(x=x, y=ypos))
-        y = nextY
-
-
-    print("WIRE 2")
-    x = 0
-    y = 0
-    for word in path2:
-        print(word)
-        (nextX, nextY, step) = calcNextPoint(word, x, y)
-        for xpos in range(x+step, nextX+step, step):
-            print("Checking ({x},{y})".format(x=xpos, y=y))
-            if ((xpos, y)) in coords:
-                print("Hit")
-                junctions.add((xpos, y))
-                coords.remove((xpos, y))
-            x = nextX
-
-        for ypos in range(y+step, nextY+step, step):
-            print("Checking ({x},{y})".format(x=x, y=ypos))
-            if ((x, ypos)) in coords:
-                print("Hit")
-                junctions.add((x, ypos))
-                coords.remove((x, ypos))
-            y = nextY
-
-
-    # Delete the (0, 0) junction
-    junctions.discard((0, 0))
-
-    # Calc distance
-    distance = calcMinDistance(junctions)
-
-    print(junctions)
-    print("Shortest distance: {distance}".format(distance=distance))
-
-    # Optional, draw map
-    pyglet.app.run()
 
 if (__name__ == '__main__'):
     main()
