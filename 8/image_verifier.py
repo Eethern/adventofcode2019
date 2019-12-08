935a9c199c9a512c382d9051fc5223787469ee1a
Christoffer Arvidsson <christoffer@arvidson.nu>
2019-12-08 15:47:28 +0100

    part 2, day 8 2019

--- 8/image_verifier.py
+++ 8/image_verifier.py
@@ -1,5 +1,6 @@
 from collections import deque
 
+
 class Layer:
     def __init__(self, width, height, data):
         self.width = width
@@ -26,6 +27,44 @@ def countDigitInLayer(layer, digit):
     return count
 
 
+def overlayLayers(layers, render):
+    width = layers[0][0].width
+    height = layers[0][0].height
+
+    # Initialize not rendered
+    for index in range(len(render)):
+        render[index] = (render[index], False)
+
+    # Loop over all layer pixels
+    for layer in layers:
+        layer = layer[0]
+        # Loop over rows
+        for rind, row in enumerate(layer.data):
+            # Loop over columns
+            for cind in range(width):
+                color = int(row[cind])
+                index = rind * width + cind
+                if ((color != 2) and (render[index][1] is False)):
+                    render[index] = (color, True)
+
+    render = [str(pixel) for (pixel, cond) in render]
+    return render
+
+
+def printImage(render, width, height):
+    for index, char in enumerate(render):
+        if char == '2':
+            render[index] = ' '
+        if char == '1':
+            render[index] = '░'
+        if char == '0':
+            render[index] = '█'
+
+    image = "".join(render)
+    for r in range(height):
+        print(image[width*r:width*(r + 1)])
+
+
 def main():
     with open('input.txt') as FileObj:
         raw_data = deque(FileObj.read().rstrip())
@@ -39,13 +78,20 @@ def main():
         layer = (layer, countDigitInLayer(layer, '0'))
         layers.append(layer)
         minLayer = layer if (minLayer is None or layer[1] < (minLayer[1])) else minLayer
-        # print(minLayer[0].data, minLayer[1])
-        # print("raw data: ", raw_data)
-        # print("length of raw_data ", len(raw_data))
 
     numOnes = countDigitInLayer(minLayer[0], '1')
     numTwos = countDigitInLayer(minLayer[0], '2')
 
     print("Result: ", numOnes * numTwos)
+
+    # Create transparent render list
+    render = [2] * width * height
+
+    render = overlayLayers(layers, render)
+    print("Decoded Image")
+    printImage(render, width, height)
+
+
+
 if (__name__ == '__main__'):
     main()
