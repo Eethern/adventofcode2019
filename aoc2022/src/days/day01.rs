e712b32bca07b556f1f895ea3a2af7d57189c9f4
Christoffer Arvidsson <christoffer@arvidson.nu>
2022-12-01 19:06:55 +0100

    day01: replace with binaryheap
    No real gain in performance, but will scale better :)

--- aoc2022/src/days/day01.rs
+++ aoc2022/src/days/day01.rs
@@ -1,25 +1,26 @@
 use crate::problem::Problem;
+use std::collections::BinaryHeap;
 
 pub struct Solution {}
 
-fn parse(input: &str) -> Vec<i32> {
+fn parse(input: &str) -> BinaryHeap<i32> {
     input
         .split("\n\n")
-        .map(|x| x.lines().fold(0, |acc, n| acc + n.parse::<i32>().unwrap())).collect()
+        .map(|x| x.lines().fold(0, |acc, n| acc + n.parse::<i32>().unwrap()))
+        .collect::<BinaryHeap<i32>>()
 }
 
 impl Problem for Solution {
     fn part1(&self, _input: &str) -> Option<String> {
         let weights = parse(&_input);
-        let max = weights.iter().max().unwrap();
+        let max = weights.peek().unwrap();
 
         Some(format!("{}", max))
     }
 
     fn part2(&self, _input: &str) -> Option<String> {
         let mut weights = parse(&_input);
-        weights.sort();
-        let answer: i32 = weights.iter().rev().take(3).sum();
+        let answer: i32 = weights.pop().unwrap() + weights.pop().unwrap() + weights.peek().unwrap();
         Some(format!("{}", answer))
     }
 }
