9a8252ad5fde82461430da27722495604860fac0
Christoffer Arvidsson <christoffer.arvidsson@zenseact.com>
2022-12-02 08:27:26 +0100

    202202ab slight improvement

--- aoc2022/src/days/day02.rs
+++ aoc2022/src/days/day02.rs
@@ -7,22 +7,22 @@ fn solve(input: &str, pointmap: &HashMap<&str, u32>) -> u32 {
     input
         .lines()
         .fold(0, |acc, l| {
-            acc + pointmap.get(&*l.replace(" ", "")).unwrap()
+            acc + pointmap.get(l).unwrap()
         })
 }
 
 impl Problem for Solution {
     fn part1(&self, _input: &str) -> Option<String> {
         let pointmap = HashMap::from([
-            ("AX", 4),
-            ("BX", 1),
-            ("CX", 7),
-            ("AY", 8),
-            ("BY", 5),
-            ("CY", 2),
-            ("AZ", 3),
-            ("BZ", 9),
-            ("CZ", 6)
+            ("A X", 4),
+            ("B X", 1),
+            ("C X", 7),
+            ("A Y", 8),
+            ("B Y", 5),
+            ("C Y", 2),
+            ("A Z", 3),
+            ("B Z", 9),
+            ("C Z", 6)
         ]);
 
         let answer = solve(&_input, &pointmap);
@@ -32,15 +32,15 @@ impl Problem for Solution {
 
     fn part2(&self, _input: &str) -> Option<String> {
         let pointmap = HashMap::from([
-            ("AX", 3),
-            ("BX", 1),
-            ("CX", 2),
-            ("AY", 4),
-            ("BY", 5),
-            ("CY", 6),
-            ("AZ", 8),
-            ("BZ", 9),
-            ("CZ", 7)
+            ("A X", 3),
+            ("B X", 1),
+            ("C X", 2),
+            ("A Y", 4),
+            ("B Y", 5),
+            ("C Y", 6),
+            ("A Z", 8),
+            ("B Z", 9),
+            ("C Z", 7)
         ]);
 
         let answer = solve(&_input, &pointmap);
