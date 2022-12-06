1fb162750f13f31000e79afd19c5b4d4b47a855c
Christoffer Arvidsson <christoffer@arvidson.nu>
2022-12-06 07:11:12 +0100

    Refactor 202206ab

--- aoc2022/src/days/day06.rs
+++ aoc2022/src/days/day06.rs
@@ -10,18 +10,22 @@ fn unique_chars(s: &[char]) -> bool {
     v.len() == y.len()
 }
 
+fn find_first_unique_group(_input: &str, group_size: usize) -> usize {
+        _input.chars().collect::<Vec<char>>()
+            .windows(group_size)
+            .into_iter()
+            .position(|w| unique_chars(w))
+            .unwrap()
+}
+
 impl Problem for Solution {
     fn part1(&self, _input: &str) -> Option<String> {
-        let v = _input.chars().collect::<Vec<char>>();
-        let windows = v.windows(4);
-        let answer = windows.into_iter().position(|w| unique_chars(w)).unwrap() + 4;
+        let answer = find_first_unique_group(_input, 4) + 4;
         Some(answer.to_string())
     }
 
     fn part2(&self, _input: &str) -> Option<String> {
-        let v = _input.chars().collect::<Vec<char>>();
-        let windows = v.windows(14);
-        let answer = windows.into_iter().position(|w| unique_chars(w)).unwrap() + 14;
+        let answer = find_first_unique_group(_input, 14) + 14;
         Some(answer.to_string())
     }
 }
@@ -39,6 +43,5 @@ mod tests {
         let answer = windows.into_iter().position(|w| unique_chars(w)).unwrap() + 4;
 
         assert_eq!(7, answer);
-        
     }
 }
