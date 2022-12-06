9154634d74d194f7b6adafc34b5b604c80b3c793
Christoffer Arvidsson <christoffer@arvidson.nu>
2022-12-06 06:57:59 +0100

    solve 202206ab

--- aoc2022/src/days/day06.rs
+++ aoc2022/src/days/day06.rs
@@ -2,14 +2,43 @@ use crate::problem::Problem;
 
 pub struct Solution {}
 
+fn unique_chars(s: &[char]) -> bool {
+    let v: Vec<char> = s.to_vec();
+    let mut y = v.clone();
+    y.sort();
+    y.dedup();
+    v.len() == y.len()
+}
+
 impl Problem for Solution {
     fn part1(&self, _input: &str) -> Option<String> {
-        // Some(format!("{}", "undefined"))
-        None
+        let v = _input.chars().collect::<Vec<char>>();
+        let windows = v.windows(4);
+        let answer = windows.into_iter().position(|w| unique_chars(w)).unwrap() + 4;
+        Some(answer.to_string())
     }
 
     fn part2(&self, _input: &str) -> Option<String> {
-        // Some(format!("{}", "undefined"))
-        None
+        let v = _input.chars().collect::<Vec<char>>();
+        let windows = v.windows(14);
+        let answer = windows.into_iter().position(|w| unique_chars(w)).unwrap() + 14;
+        Some(answer.to_string())
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    const INPUT: &str = "mjqjpqmgbljsphdztnvjfqwrcgsmlb";
+
+    #[test]
+    fn test_day06a() {
+        let v = INPUT.chars().collect::<Vec<char>>();
+        let windows = v.windows(4);
+        let answer = windows.into_iter().position(|w| unique_chars(w)).unwrap() + 4;
+
+        assert_eq!(7, answer);
+        
     }
 }
