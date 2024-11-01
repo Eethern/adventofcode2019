b964d99f5cd440899b743aeaf02f9db4a74986de
Christoffer Arvidsson <christoffer@arvidson.nu>
2024-11-01 17:12:41 +0100

    2020: Add ability to run specific day

--- aoc2020/src/main.cpp
+++ aoc2020/src/main.cpp
@@ -5,6 +5,24 @@
 #include "problem.h"
 
 int main() {
+    const char* day_str{std::getenv("DAY")};
+
+    if (day_str == nullptr) {
+        std::cerr << "Environment variable DAY is not set. Please provide a valid day.";
+        return 1;
+    }
+
+    size_t supplied_day;
+    try {
+        supplied_day = std::stoul(day_str);
+    } catch (const std::invalid_argument& e) {
+        std::cerr << "Invalid day format. Please provide a valid day.";
+        return 1;
+    } catch (const std::out_of_range& e) {
+        std::cerr << "Day value is out of range. Please provide a valid day.";
+        return 1;
+    }
+
     std::vector<Problem*> days{};
     days.push_back(new Day01("inputs/01.txt"));
     days.push_back(new Day02("inputs/02.txt"));
@@ -32,11 +50,19 @@ int main() {
     // days.push_back(new Day24("inputs/24.txt"));
     // days.push_back(new Day25("inputs/25.txt"));
 
-    size_t day = 1;
-    for (Problem*& d : days) {
-        std::cout << "Day " << day << "\n";
-        d->run();
-        day++;
+    if (supplied_day == 0U) {
+        for (Problem *&d : days) {
+            std::cout << "Day " << supplied_day+1U << "\n";
+            d->run();
+            supplied_day++;
+        }
+    } else if (supplied_day <= days.size()) {
+        std::cout << "Day " << supplied_day << std::endl;
+        days[supplied_day-1]->run();
+
+    } else {
+        std::cerr << "Invalid day specified. Please provide a valid day.";
+        return 1;
     }
 
     return 0;
